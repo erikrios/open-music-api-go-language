fa6bf95b5a638bb786b9210faee412ff46a3971e
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-08-20 22:24:34 +0700

    Refactor the code

--- src/services/inmemory/songs/songs.go
+++ src/services/inmemory/songs/songs.go
@@ -53,18 +53,17 @@ func GetSong(id string) (*Song, errors.Error) {
 }
 
 func UpdateSong(id string, title string, year uint16, performer string, genre string, duration uint16) errors.Error {
-	song, err := GetSong(id)
-
-	if err != nil {
-		return err
+	for i, song := range songs {
+		if id == song.Id {
+			songs[i].Title = title
+			songs[i].Year = year
+			songs[i].Performer = performer
+			songs[i].Genre = genre
+			songs[i].Duration = duration
+			return nil
+		}
 	}
-
-	song.Title = title
-	song.Year = year
-	song.Performer = performer
-	song.Genre = genre
-	song.Duration = duration
-	return nil
+	return errors.NewNotFound(fmt.Sprintf("Song with id %s not found.", id))
 }
 
 func DeleteSong(id string) errors.Error {
