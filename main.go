db2b79353b221ac838da55b31098e661ee1c5a5d
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-08-20 14:28:01 +0700

    Create simple fiber app

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"github.com/erikrios/open-music-api-go-language/src/api/home"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
+	home.HomeRoutes(app)
 
+	if err := app.Listen("3000"); err != nil {
+		fmt.Println("Failed to start the server:", err)
+	}
+
+	fmt.Println("Server starting on port", 3000)
 }
