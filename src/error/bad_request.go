47e678c7e2361f26bb9684d32c3804c53bfef52e
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-08-20 13:33:17 +0700

    Create bad_request.go

---
+++ src/error/bad_request.go
@@ -0,0 +1,32 @@
+package error
+
+// BadRequest a struct for 400 Response Error
+type BadRequest struct {
+	name       string
+	message    string
+	statusCode uint16
+}
+
+// NewBadRequest is a function to create the instance of Bad Request struct
+func NewBadRequest(message string) BadRequest {
+	return BadRequest{
+		name:       "Bad Request",
+		message:    message,
+		statusCode: 400,
+	}
+}
+
+// Name is a method to get the name of the Error
+func (b BadRequest) Name() string {
+	return b.name
+}
+
+// Message is a method to get the message of the Error
+func (b BadRequest) Message() string {
+	return b.message
+}
+
+// StatusCode is a method to get the status code of the Error
+func (b BadRequest) StatusCode() uint16 {
+	return b.statusCode
+}
