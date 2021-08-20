7aa5c303da10c48fa21e5bb728a180cad9198530
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-08-20 16:21:29 +0700

    Create response.go

--- src/api/home/handler.go
+++ src/api/home/handler.go
@@ -1,13 +1,25 @@
 package home
 
-import "github.com/gofiber/fiber/v2"
+import (
+	_ "github.com/erikrios/open-music-api-go-language/docs"
+	"github.com/gofiber/fiber/v2"
+)
 
+// GetHome godoc
+// @Summary Get hello world message
+// @Description get hello world message from the server
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} Success
+// @Failure 404,500 {object} Error
+// @Failure default {object} Error
+// @Router / [get]
 func getHome(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"status":  "success",
-		"message": nil,
-		"data": fiber.Map{
-			"message": "Hello, World!",
+	return c.JSON(Success{
+		Status:  "success",
+		Message: nil,
+		Data: Data{
+			Message: "Hello, World!",
 		},
 	})
 }
