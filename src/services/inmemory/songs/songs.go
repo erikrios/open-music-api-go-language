0484dcad2b5664ca01dd0f26b458ce9f41c41800
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-08-20 20:07:46 +0700

    Refactor the code

--- src/services/inmemory/songs/songs.go
+++ src/services/inmemory/songs/songs.go
@@ -8,38 +8,30 @@ import (
 )
 
 type Song struct {
-	Id         string        `json:"id"`
-	Title      string        `json:"title"`
-	Year       uint16        `json:"year"`
-	Performer  string        `json:"performer"`
-	Genre      string        `json:"genre"`
-	Duration   time.Duration `json:"duration"`
-	InsertedAt string        `json:"insertedAt"`
-	UpdatedAt  string        `json:"updatedAt"`
-}
-
-type Payload struct {
-	Title     string        `json:"title"`
-	Year      uint16        `json:"year"`
-	Performer string        `json:"performer"`
-	Genre     string        `json:"genre"`
-	Duration  time.Duration `json:"duration"`
+	Id         string
+	Title      string
+	Year       uint16
+	Performer  string
+	Genre      string
+	Duration   time.Duration
+	InsertedAt string
+	UpdatedAt  string
 }
 
 var songs = make([]Song, 0)
 
-func AddSong(payload Payload) string {
+func AddSong(title string, year uint16, performer string, genre string, duration time.Duration) string {
 	id, _ := nanoid.Generate(nanoid.DefaultAlphabet, 16)
 	insertedAt := time.Now().Format(time.RFC3339)
 	updatedAt := insertedAt
 
 	song := Song{
 		Id:         id,
-		Title:      payload.Title,
-		Year:       payload.Year,
-		Performer:  payload.Performer,
-		Genre:      payload.Genre,
-		Duration:   payload.Duration,
+		Title:      title,
+		Year:       year,
+		Performer:  performer,
+		Genre:      genre,
+		Duration:   duration,
 		InsertedAt: insertedAt,
 		UpdatedAt:  updatedAt,
 	}
@@ -47,20 +39,8 @@ func AddSong(payload Payload) string {
 	return id
 }
 
-func GetSongs() []map[string]interface{} {
-	results := make([]map[string]interface{}, 0)
-
-	for _, song := range songs {
-		result := map[string]interface{}{
-			"id":        song.Id,
-			"title":     song.Title,
-			"performer": song.Performer,
-		}
-
-		results = append(results, result)
-	}
-
-	return results
+func GetSongs() []Song {
+	return songs
 }
 
 func GetSong(id string) (*Song, errors.Error) {
@@ -72,22 +52,22 @@ func GetSong(id string) (*Song, errors.Error) {
 	return nil, errors.NewNotFound(fmt.Sprintf("Song with id %s not found.", id))
 }
 
-func UpdateSong(id string, payload Payload) errors.Error {
+func UpdateSong(id string, title string, year uint16, performer string, genre string, duration time.Duration) errors.Error {
 	song, err := GetSong(id)
 
 	if err != nil {
 		return err
 	}
 
-	song.Title = payload.Title
-	song.Year = payload.Year
-	song.Performer = payload.Performer
-	song.Genre = payload.Genre
-	song.Duration = payload.Duration
+	song.Title = title
+	song.Year = year
+	song.Performer = performer
+	song.Genre = genre
+	song.Duration = duration
 	return nil
 }
 
-func DeleteSong(id string, payload Payload) errors.Error {
+func DeleteSong(id string) errors.Error {
 	for i, song := range songs {
 		if id == song.Id {
 			songs = remove(songs, i)
