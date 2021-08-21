e807738d1c421dab376c07a04ee37c9fe9155023
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-08-21 20:59:52 +0700

    Create GetSongs function to get songs in the database

--- src/services/postgresql/songs/songs.go
+++ src/services/postgresql/songs/songs.go
@@ -1,6 +1,7 @@
 package songs
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/aidarkhanov/nanoid"
 	"github.com/erikrios/open-music-api-go-language/src/database"
@@ -19,6 +20,7 @@ type Song struct {
 	UpdatedAt  string  `json:"updatedAt"`
 }
 
+// AddSong is a function to add a song into the database
 func AddSong(title string, year uint16, performer string, genre *string, duration *uint16) (string, errors.Error) {
 	nanoidId, _ := nanoid.Generate(nanoid.DefaultAlphabet, 16)
 	id := fmt.Sprintf("song-%s", nanoidId)
@@ -40,4 +42,46 @@ func AddSong(title string, year uint16, performer string, genre *string, duratio
 		return "", errors.NewInternalServerError("Something went wrong.")
 	}
 	return id, nil
+}
+
+// GetSongs is a function to get songs in the database
+func GetSongs() ([]Song, errors.Error) {
+	statement := `SELECT id, title, year, performer, genre, duration, inserted_at, updated_at
+					FROM songs`
+
+	db, err := database.Db()
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println(err)
+		return nil, errors.NewInternalServerError("Something went wrong.")
+	}
+
+	rows, err := db.Query(statement)
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.NewInternalServerError("Something went wrong.")
+	}
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(rows)
+
+	var songs = make([]Song, 0)
+	for rows.Next() {
+		var song Song
+		err := rows.Scan(&song.Id, &song.Title, &song.Year, &song.Performer, &song.Genre, &song.Duration, &song.InsertedAt, &song.UpdatedAt)
+		if err != nil {
+			fmt.Println(err)
+			return nil, errors.NewInternalServerError("Something went wrong.")
+		}
+		songs = append(songs, song)
+	}
+	if err := rows.Err(); err != nil {
+		fmt.Println(err)
+		return nil, errors.NewInternalServerError("Something went wrong.")
+	}
+
+	return songs, nil
 }
