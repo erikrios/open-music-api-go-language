0b3fb8aaf848e7a0fac4e22eed2c9a7ca1458965
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-08-21 23:03:01 +0700

    Create ErrorHandler function to handle the response error

--- src/errors/error.go
+++ src/errors/error.go
@@ -1,8 +1,31 @@
 package errors
 
+import "github.com/gofiber/fiber/v2"
+
 // Error an interface to define the Response Error
 type Error interface {
 	Name() string
 	Message() string
 	StatusCode() uint16
+}
+
+// ErrorHandler is a function to handle the response error
+func ErrorHandler(err Error, c *fiber.Ctx) error {
+	status := "fail"
+	var statusCode uint16
+
+	switch err.(type) {
+	case BadRequest:
+		statusCode = fiber.StatusBadRequest
+	case NotFound:
+		statusCode = fiber.StatusNotFound
+	default:
+		status = "error"
+		statusCode = fiber.StatusInternalServerError
+	}
+
+	return c.Status(int(statusCode)).JSON(fiber.Map{
+		"status":  status,
+		"message": err.Message(),
+	})
 }
