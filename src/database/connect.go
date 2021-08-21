e14cff2fe4068088b37e4a935302da9a65343f4a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-08-21 11:44:44 +0700

    Create connect.go

---
+++ src/database/connect.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"os"
+	"strconv"
+)
+
+func Connect() (*sql.DB, error) {
+	host := os.Getenv("DB_HOST")
+	port, _ := strconv.Atoi(os.Getenv("DB_PORT"))
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbName,
+	)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, fmt.Errorf("connection to database failed: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		defer func(db *sql.DB) {
+			if err := db.Close(); err != nil {
+				fmt.Printf("failed to close the connection: %v", err)
+			}
+		}(db)
+		return nil, fmt.Errorf("can't sent ping to database: %w", err)
+	}
+
+	fmt.Println("Successfully connected to database!")
+	return db, nil
+}
